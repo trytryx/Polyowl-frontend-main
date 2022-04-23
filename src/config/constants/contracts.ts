7118973d62e1bcf1a9dbbd566325e977e9c9548a
trytryx <100495047+trytryx@users.noreply.github.com>
2022-04-23 20:29:25 +0300

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,9 +1,9 @@
 export default {
   cake: {
-    137: '0x3105A8707c8E81BfA3eacB788373e58C227b5929'
+    137: '0xc7553307eD737dcE132aE7915E6496864617B261'
   },
   masterChef: {
-    137: '0x888512da81f9410a37b1418806bCF6E0D510f4e2',
+    137: '0x0621Db5cdDc1126B1E17087fF647993B5A156F34',
   },
   weth: {
     137: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619"
@@ -17,7 +17,7 @@ export default {
     97: '',
   },
   mulltiCall: {
-    137: '0x9273a5E2FB9df10dd4A2A0Ba27d2aaC876F2470a'
+    137: '0x40524425bcb58Dc16f11746B0028e23aCDA1e128'
   },
   usdc: {
     137: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
